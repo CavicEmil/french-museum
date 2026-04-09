96351dbd6da2b669249db2a9199f5a672c5e1f78
CavicEmil <eaa25emca@students.eaaa.dk>
2026-04-09 09:35:59 +0200

    added header and footer components

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,7 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Background from './components/Background';
 
 export default function Layout() {
     return (
---
+++ src/components/Header.jsx
@@ -0,0 +1,5 @@
+export default function Header() {
+    return (
+        <h1>Bonjour this is header</h1>
+    )
+}
