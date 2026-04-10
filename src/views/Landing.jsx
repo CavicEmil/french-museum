4bdc2bdf63e2b43bc05c1db3363af2bb2ab31238
Julia <eaa25juma@students.eaaa.dk>
2026-04-10 09:13:05 +0200

    Opening Times and Admission

--- src/views/Landing.jsx
+++ src/views/Landing.jsx
@@ -2,12 +2,24 @@ import Hero from '../components/Hero';
 
 export default function Landing() {
     return (
-        <div>
-            <h1 class>
-                kunsthalaarhus
-            </h1>
-            <p>Kunsthal Aarhus is an inclusive exhibition space that aims to communicate clearly, enthusiastically, and engagingly to a broad audience, sparking curiosity about contemporary art. </p>
-            <Hero />
-        </div>
+        <>
+            <div>
+                <h1 className="landing-title">
+                    kunsthalaarhus
+                </h1>
+                <p>Kunsthal Aarhus is an inclusive exhibition space that aims to communicate clearly, enthusiastically, and engagingly to a broad audience, sparking curiosity about contemporary art. </p>
+                <Hero />
+            </div>
+
+            <div>
+                <p>Tirsdag 11-18</p>
+                <p>Onsdag 11-18</p>
+                <p>Torsdag 11-20</p>
+                <p>Fredag 11-18</p>
+                <p>Lørdag 11-18</p>
+                <p>Søndag 11-18</p>
+                <h2>Fri entré</h2>
+            </div>
+        </>
     )
 }
